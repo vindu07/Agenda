21a2c1444c7d6332f6992ee8dd453a83921aaf39
vindu07 <andrea.vinduska@gmail.com>
2025-10-07 18:00:15 +0200

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,1 +1,23 @@
 
+import * as db from "./db.js";
+import * as Tasks from "./tasks.js";
+import * as settings from "./settings.js";
+import { Timestamp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-firestore.js";
+
+const numGiorni = 3; //quanti giorni visualizza
+
+let oggi = Timestamp.fromDate(new Date());
+let termine = new Date(); termine.setDate(termine.getDate() + numGiorni); termine = Timestamp.fromDate(termine);
+
+const parametri = { isCompleted: false ,dataInizio: oggi, dataFine: termine, collection: "tasks"}
+
+
+db.sortTasks(parametri)
+  .then(sortedTasks => {
+    // qui sortedTasks è l'array ordinato
+    Tasks.renderTasks(sortedTasks);
+  })
+  .catch(err => {
+    console.error("Errore nel caricamento/ordinamento dei task:", err);
+  });
+
