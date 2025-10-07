178626f161a5bb3fd17577fe422e1c6cf05bcf8d
vindu07 <andrea.vinduska@gmail.com>
2025-10-07 22:10:28 +0200

    Update new-task.js

--- js/new-task.js
+++ js/new-task.js
@@ -1,1 +1,43 @@
+/*MOSTRA/NASCONDE HUD*/
+document.addEventListener("DOMContentLoaded", () => {
+  const newTask = document.getElementById("new-task");
+  newTask.addEventListener("click", () => {
+    const hud = document.getElementById("hud"); 
+    hud.classList.toggle("invisible");
+  });
 
+  const annulla = document.getElementById("annulla-task");
+  annulla.addEventListener("click", () => {
+    const hud = document.getElementById("hud"); 
+    hud.classList.toggle("invisible");
+  });
+});
+
+/*SALVA TASK*/
+
+document.getElementById("save-task").addEventListener("click", () => {
+  
+  // raccogli valori dall'HUD
+  const scadenza = new Date(pagDiario);
+  const isCompleted = false;
+  const desc = document.getElementById("task-desc").value.trim();
+  const materia = document.getElementById("task-subject").value;
+  const isTest = document.getElementById("isTest").value === "0" ? false : true;
+  const priority = parseInt(document.getElementById("task-priority").value);
+  
+  //chiudi hud
+  const hud = document.getElementById("hud"); 
+    hud.classList.toggle("invisible");
+
+  // costruisci un array task
+  const newTask = [ scadenza, materia, isTest, priority, desc, isCompleted  ];
+
+  //salva in firestore
+  db.createTask(newTask);
+
+  const salva = document.getElementById("save-task");
+  salva.addEventListener("click", () => {
+    const hud = document.getElementById("hud"); 
+    hud.classList.toggle("invisible");
+  });
+});
