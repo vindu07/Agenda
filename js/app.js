350245d7986f7b6979504bc2775066e8056f1387
vindu07 <andrea.vinduska@gmail.com>
2025-10-07 18:05:27 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,75 @@
 
+import * as db from "./db.js";
+
+
+/*CAMBIA TEMA*/
+document.addEventListener("DOMContentLoaded", () => {
+  const toggleBtn = document.getElementById("toggle-theme");
+  toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("light-mode");
+  });
+});
+
+/*IMPOSTAZIONI*/
+document.addEventListener("DOMContentLoaded", () => {
+  const settingsBtn = document.getElementById("settings");
+  settingsBtn.addEventListener("click", () => {
+    //da implementare
+  });
+});
+
+/*DASHBOARD*/
+document.addEventListener("DOMContentLoaded", () => {
+  const settingsBtn = document.getElementById("dashboard");
+  settingsBtn.addEventListener("click", () => {
+    //da implementare
+  });
+});
+
+/*CALENDARIO*/
+document.addEventListener("DOMContentLoaded", () => {
+  const settingsBtn = document.getElementById("calendar");
+  settingsBtn.addEventListener("click", () => {
+    //da implementare
+  });
+});
+
+/*AGENDA*/
+document.addEventListener("DOMContentLoaded", () => {
+  const settingsBtn = document.getElementById("diary");
+  settingsBtn.addEventListener("click", () => {
+    //da implementare
+  });
+});
+
+/*ORARIO*/
+document.addEventListener("DOMContentLoaded", () => {
+  const settingsBtn = document.getElementById("timetable");
+  settingsBtn.addEventListener("click", () => {
+    //da implementare
+  });
+});
+
+
+// registra il service worker
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("sw.js")
+    .then(() => console.log("Service Worker registrato"))
+    .catch(err => console.error("Errore Service Worker:", err));
+}
+
+/*REFRESHA LA CACHE DEL BROWSER*/
+if (navigator.onLine && navigator.serviceWorker.controller) {
+  navigator.serviceWorker.controller.postMessage("refreshCache");
+} // all'avvio
+
+setInterval(() => {
+  if (navigator.online && navigator.serviceWorker.controller) {
+    navigator.serviceWorker.controller.postMessage("refreshCache");
+  }
+}, 10 * 60 * 1000); // ogni 10 minuti
+
+
+
+db.archiveTasks(); //archivia i task passati
+
