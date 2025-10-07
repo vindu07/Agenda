61b10173c44fbaf85a8e6db5a290756a8a8127af
vindu07 <andrea.vinduska@gmail.com>
2025-10-07 18:00:46 +0200

    Update calendar.js

--- js/calendar.js
+++ js/calendar.js
@@ -1,1 +1,16 @@
 
+const days = document.querySelectorAll("#calendar .day-number");
+
+// metto i numeri dei giorni
+days.forEach((el, i) => {
+  if(i<=3){ el.textContent = i + 1;} // esempio: 1..35
+});
+
+// aggiungo 3 pallini al giorno 10
+const dotsDiv = document.querySelectorAll("#calendar .tasks")[9];
+["red", "green", "blue"].forEach(c => {
+  const d = document.createElement("div");
+  d.className = `dot ${c}`;
+  dotsDiv.appendChild(d);
+});
+
